222dc04d142ab1f6288692d5b6f26f2cf5541408
Ali Mohaghegh <mohaghegh.ali76@gmail.com>
2023-05-12 18:16:32 -0500

    V1.0.0
    Initial Commit
    Virtual Enviroment is created.
    ROMify is a 1D compressible reacting flow solver with reduced-order-model(ROM) features. It aims to provide a simple and easy-to-use testbed to quickly prototype and examine new ROM methods

---
+++ main.py
@@ -0,0 +1,2 @@
+# ROMify is a 1D compressible reacting flow solver with reduced-order-model(ROM) features.
+# It aims to provide a simple and easy-to-use testbed to quickly prototype and examine new ROM methods
